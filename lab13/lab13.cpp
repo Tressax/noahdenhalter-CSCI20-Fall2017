22f33546052ade734a836fa0ce08255e4039e4de
Noah Denhalter <ndenhalter001@student.butte.edu>
2017-08-30 20:46:06 +0000

    Lab1.3 submission

---
+++ lab13/lab13.cpp
@@ -0,0 +1,34 @@
+//Noah Denhalter
+//8/30/2017
+//Future population machine
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+ 
+int main()
+{
+    //Population variables
+    double current_population = 325758706;
+    double birth_rate = 8;
+    double death_rate = 12;
+    double migration_rate = 33;
+    double current_year = 2017;
+    
+    //Population growth rate per minute equation
+    birth_rate = 60/birth_rate;
+    death_rate = 60/death_rate;
+    migration_rate = 60/migration_rate;
+
+    //population per year equation
+    double population_growth_rate = birth_rate + migration_rate - death_rate;
+    
+    double population_per_year = ((population_growth_rate * 60) * 24) * 365;
+
+    //Determining future population
+    int years_passed = 99;
+    int future_population = round((years_passed * population_per_year) + current_population);
+    
+    //declaring future population
+    cout<<"In the year "<<current_year + years_passed<<", the population will be "<<future_population<<" in the United States."<<endl;
+}
